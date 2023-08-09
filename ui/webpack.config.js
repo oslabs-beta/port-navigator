c2f24897e6b0e9e83b6451dca1aebc266d42c94e
Clayton Stewart <claytons1212@gmail.com>
2023-08-09 09:35:23 -0600

    converted react-scripts to webpack

---
+++ ui/webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.tsx',
+  mode: process.env.NODE_ENV,
+  resolve: {
+    extensions: ['.jsx', '.js', '.ts', '.tsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: [/node_modules/],
+        use: ['ts-loader'],
+      },
+      {
+        test: /.(css|scss)$/,
+        exclude: [/node_modules/],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html',
+    }),
+  ],
+  devServer: {
+    host: 'localhost',
+    port: 3000,
+    hot: true,
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, './dist'),
+    },
+  },
+};
