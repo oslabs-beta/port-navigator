d380f1bc020f6ef46832fb0a5b54fdc0017ec17a
Clayton Stewart <claytons1212@gmail.com>
2023-08-19 13:09:26 -0600

    build works now without running server

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -2,24 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import CssBaseline from '@mui/material/CssBaseline';
 import { DockerMuiThemeProvider } from '@docker/docker-mui-theme';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './styles.css';
-import { App } from './App';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/*
-      If you eject from MUI (which we don't recommend!), you should add
-      the `dockerDesktopTheme` class to your root <html> element to get
-      some minimal Docker theming.
-    */}
     <DockerMuiThemeProvider>
       <CssBaseline />
-      <BrowserRouter>
+      <HashRouter basename=''>
         <App />
-      </BrowserRouter>
+      </HashRouter>
     </DockerMuiThemeProvider>
-    ,
   </React.StrictMode>,
   document.getElementById('root'),
 );
